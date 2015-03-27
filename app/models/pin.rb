3dda4f2c2fc93e703fb36e293788a4b9932470e8
Vinodhini Ravikumar <vinradme@gmail.com>
2015-03-27 11:27:44 -0700

    Load pins in index action in reverse order

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,9 @@
 class Pin < ActiveRecord::Base
   belongs_to :user
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                    :storage => :s3,
+                    :bucket => ENV['S3_BUCKET_NAME']
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 end
